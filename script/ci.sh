ac05db55b92ea829d85768c199fb5fb5ab7f6d72
Ryan Neal <ry.neal@gmail.com>
2016-12-07 10:47:28 -0800

    update to not have a volume mount

--- script/ci.sh
+++ script/ci.sh
@@ -6,23 +6,8 @@ set -x
 PROJECT=speedy
 WORKSPACE=/go/src/github.com/netlify/$PROJECT
 
-#
-# start dependent containers
-#
-DB_VOLUME=`docker volume create`
-
-#
-# cleanup other containers
-#
-function cleanup {
-	docker volume rm ${DB_VOLUME} || true
-	rm -rf vendor
-}
-trap cleanup EXIT
-
 docker run \
 	--volume $(pwd):$WORKSPACE \
-	--volume $DB_VOLUME:$WORKSPACE/vendor \
 	--workdir $WORKSPACE \
 	--rm \
 	calavera/go-glide:v0.12.2 script/test.sh $PROJECT
